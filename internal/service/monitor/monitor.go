97a9bc3721412bd105e671ba638ce391c28e154c
zhangz1w3nCode <403592973@qq.com>
2024-09-16 22:07:46 +0800

    feat:add monitor rpc interface.

--- internal/service/monitor/monitor.go
+++ internal/service/monitor/monitor.go
@@ -21,11 +21,9 @@ func NewMonitorService(manager *manager.CacheManager) *MonitorService {
 func (m *MonitorService) GetCacheUserIpList(ctx context.Context, req *monitorpb.GetCacheUserIpListRequest) (*monitorpb.GetCacheUserIpListResponse, error) {
 	return &monitorpb.GetCacheUserIpListResponse{UserCacheIpList: make([]string, 0)}, nil
 }
-
 func (m *MonitorService) GetCacheUserAppNameList(ctx context.Context, req *monitorpb.GetCacheUserAppNameListRequest) (*monitorpb.GetCacheUserAppNameListResponse, error) {
 	return &monitorpb.GetCacheUserAppNameListResponse{UserCacheAppNameList: make([]string, 0)}, nil
 }
-
 func (m *MonitorService) GetCacheNameList(ctx context.Context, req *monitorpb.GetCacheNameListRequest) (*monitorpb.GetCacheNameListResponse, error) {
 	return &monitorpb.GetCacheNameListResponse{CacheNameList: make([]string, 0)}, nil
 }
