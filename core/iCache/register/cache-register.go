9685ff5fc894876fb1928e447fb7f69b9f7ebb20
zhangz1w3nCode <403592973@qq.com>
2024-09-19 21:07:43 +0800

    feat:add clear up

--- core/iCache/register/cache-register.go
+++ core/iCache/register/cache-register.go
@@ -126,7 +126,7 @@ func ServerPostHandler(serverName string, serverAddress string, zkAddress string
 	if err = zkConn.Delete(path, int32(0)); err != nil {
 		log.Printf("Delete services.%s.%s error!", serverName, serverAddress)
 	}
-
+	log.Printf("Delete services.%s.%s successful!", serverName, serverAddress)
 	//services节点下的serverName节点的具体机器注册的节点是否存在
 	//exists, stat, err = zkConn.Exists("/services/" + serverName)
 	//
@@ -150,9 +150,10 @@ func ServerPostHandler(serverName string, serverAddress string, zkAddress string
 		log.Printf("Get path resource from zookeeper stat error! %v", err)
 	}
 	if !exists {
-		log.Printf("services.%s.%s has been deleted!", serverName, serverAddress)
+		log.Printf("services.%s has been deleted!", serverName)
 		return
 	}
+	log.Printf("services.%s exist!", serverName)
 
 	children, stat, err := zkConn.Children("/services/" + serverName)
 	if err != nil {
@@ -161,12 +162,15 @@ func ServerPostHandler(serverName string, serverAddress string, zkAddress string
 	if stat == nil {
 		log.Printf("Get path resource from zookeeper stat error! %v", err)
 	}
+	log.Printf("services.%s children:", children)
 	if len(children) == 0 {
+		log.Printf("services.%s children nums is 0!", serverName)
 		//删除services节点下的serverName节点
 		path = "/services/" + serverName
 		if err = zkConn.Delete(path, int32(0)); err != nil {
 			log.Printf("Delete services.%s error!", serverName)
 		}
+		log.Printf("Delete %sservices.%s%s successful!", "[", serverName, "]")
 	}
 
 	log.Printf("Delete %sservices.%s.%s%s successful!", "[", serverName, serverAddress, "]")
