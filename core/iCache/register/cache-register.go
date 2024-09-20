8ee167288255cd0569aa20e926d134e99e531b37
zhangz1w3nCode <403592973@qq.com>
2024-09-21 00:11:18 +0800

    feat:zookeeper to etcd.

--- core/iCache/register/cache-register.go
+++ core/iCache/register/cache-register.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"github.com/samuel/go-zookeeper/zk"
 	"github.com/zhangz1w3nCode/go-iCache/config"
+	"github.com/zhangz1w3nCode/go-iCache/core/iCache/etcd"
 	"github.com/zhangz1w3nCode/go-iCache/core/iCache/manager"
 	monitorsvc "github.com/zhangz1w3nCode/go-iCache/internal/service/monitor"
 	monitorpb "github.com/zhangz1w3nCode/go-iCache/pb/generate/cache-monitor"
@@ -53,7 +54,7 @@ func RegisterZookeeper(zookeeperServers []string, serviceName string, ip string,
 	return nil
 }
 
-func RegisterCacheServcie(s *grpc.Server, serviceName, bizAppIp string, zkIp string, managerCache *manager.CacheManager) {
+func RegisterCacheServcie(s *grpc.Server, serviceName, bizAppIp string, etcdAddress string, managerCache *manager.CacheManager) *etcd.EtcdRegister {
 	//读取配置文件地址设置
 	config.Init("./config/config.yaml")
 
@@ -61,12 +62,19 @@ func RegisterCacheServcie(s *grpc.Server, serviceName, bizAppIp string, zkIp str
 	monitorpb.RegisterCacheMonitorServiceServer(s, monitorService)
 
 	info := s.GetServiceInfo()
+	jsonStr, err := json.Marshal(info)
 
-	errZk := RegisterZookeeper([]string{zkIp}, serviceName, bizAppIp, info)
-
-	if errZk != nil {
-		log.Fatalf("failed to start zookeeper: %v", errZk)
+	//errZk := RegisterZookeeper([]string{etcdAddress}, serviceName, bizAppIp, info)
+	etcdRegister, err := etcd.NewEtcdRegister(etcdAddress)
+	if err != nil {
+		log.Fatalf("failed to get etcd connect: %v", err)
 	}
+	err = etcdRegister.ServiceRegister("/services/"+serviceName+"/"+bizAppIp, string(jsonStr), 60)
+	if err != nil {
+		log.Fatalf("failed to register etcd: %v", err)
+	}
+
+	return etcdRegister
 }
 
 // ServerPostHandler 关闭资源
@@ -126,20 +134,6 @@ func ServerPostHandler(serverName string, serverAddress string, zkAddress string
 	if err = zkConn.Delete(path, int32(0)); err != nil {
 		log.Printf("Delete services.%s.%s error!", serverName, serverAddress)
 	}
-
-	//services节点下的serverName节点的具体机器注册的节点是否存在
-	//exists, stat, err = zkConn.Exists("/services/" + serverName)
-	//
-	//if err != nil {
-	//	log.Fatalf("Get path resource from zookeeper stat error! %v", err)
-	//}
-	//if stat == nil {
-	//	log.Fatalf("Get path resource from zookeeper stat error! %v", err)
-	//}
-	//if !exists {
-	//	log.Printf("services.%s.%s has been deleted!", serverName, serverAddress)
-	//	return
-	//}
 
 	exists, stat, err = zkConn.Exists("/services/" + serverName)
 
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/dgraph-io/ristretto v0.1.1
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/samuel/go-zookeeper v0.0.0-20201211165307-7117e9ea2414
+	go.etcd.io/etcd/client/v3 v3.5.16
 	google.golang.org/grpc v1.66.2
 	google.golang.org/protobuf v1.34.2
 	gopkg.in/yaml.v3 v3.0.1
@@ -13,11 +14,21 @@ require (
 
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
 	github.com/dustin/go-humanize v1.0.0 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/glog v1.2.1 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.16 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.16 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.17.0 // indirect
 	golang.org/x/net v0.26.0 // indirect
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240604185151-ef581f913117 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240604185151-ef581f913117 // indirect
 )
