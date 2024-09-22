7fa05c94a6d3e78b2a1ab3603923d6b0d9688041
zhangz1w3nCode <403592973@qq.com>
2024-09-23 04:32:30 +0800

    feat:add some ablity to go-iCache.

--- core/iCache/manager/manager.go
+++ core/iCache/manager/manager.go
@@ -1,10 +1,12 @@
 package manager
 
 import (
+	"fmt"
 	"github.com/zhangz1w3nCode/go-iCache/core/iCache"
 	"github.com/zhangz1w3nCode/go-iCache/core/iCache/config"
 	"github.com/zhangz1w3nCode/go-iCache/core/iCache/factory"
 	"github.com/zhangz1w3nCode/go-iCache/core/iCache/factory/imp"
+	"log"
 )
 
 // CacheManager 缓存管理器
@@ -50,5 +52,9 @@ func (m *CacheManager) GetAllCacheName() []string {
 	return cacheNameList
 }
 func (m *CacheManager) GetCacheDetail() map[string]iCache.ICache {
+	log.Println(m.cacheMap)
+	for key := range m.cacheMap {
+		fmt.Println(m.configMap[key])
+	}
 	return m.cacheMap
 }
