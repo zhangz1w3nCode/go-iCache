8ebf83bd25d44d5d7e3469c184c03917ecd94868
zhangz1w3nCode <403592973@qq.com>
2024-09-24 17:56:42 +0800

    feat:add cache status ability.

--- core/iCache/value-wrapper/valueWrapper.go
+++ core/iCache/value-wrapper/valueWrapper.go
@@ -2,22 +2,24 @@ package value_wrapper
 
 import (
 	"time"
+	"unsafe"
 )
 
 // ValueWrapper 缓存的数据包装类
 type ValueWrapper struct {
-	Data        interface{} // 缓存的数据
-	WriteTime   int64       // 缓存写入时间
-	AccessTime  int64       // 缓存最后访问时间
-	CacheStatus *CacheStatus
+	Data        interface{}  // 缓存的数据
+	WriteTime   int64        // 缓存写入时间
+	AccessTime  int64        // 缓存最后访问时间
+	CacheStatus *CacheStatus // 缓存状态
 }
 
 type CacheStatus struct {
-	CacheCurrentSize int64
-	CacheMaxSize     int64
-	CacheHit         int64
-	CacheMiss        int64
-	CacheQuery       int64
+	CacheCurrentNum int64
+	CacheMaxNum     int64
+	CacheHit        int64
+	CacheMiss       int64
+	CacheQuery      int64
+	CacheSize       int64 //缓存占用的内存大小
 }
 
 // NewValueWrapper 创建一个新的ValueWrapper实例
@@ -27,11 +29,12 @@ func NewValueWrapper(data interface{}) *ValueWrapper {
 		WriteTime:  time.Now().Unix(),
 		AccessTime: time.Now().Unix(),
 		CacheStatus: &CacheStatus{
-			CacheCurrentSize: 0,
-			CacheMaxSize:     0,
-			CacheHit:         0,
-			CacheMiss:        0,
-			CacheQuery:       0,
+			CacheCurrentNum: 0,
+			CacheMaxNum:     0,
+			CacheHit:        0,
+			CacheMiss:       0,
+			CacheQuery:      0,
+			CacheSize:       0,
 		},
 	}
 }
@@ -51,5 +54,6 @@ func (vw *ValueWrapper) UpdateCacheStatus() {
 	currentStatus := vw.CacheStatus
 	currentStatus.CacheQuery++
 	currentStatus.CacheHit++
-	currentStatus.CacheCurrentSize++
+	currentStatus.CacheCurrentNum++
+	currentStatus.CacheSize = int64(unsafe.Sizeof(&vw.Data))
 }
